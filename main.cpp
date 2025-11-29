5ffbbdf7370edc3b081657dcca1712cb39522bd6
Kiineri04 <ahmadrafi10293@gmail.com>
2025-11-29 13:43:05 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
-// main.cpp
 // Program utama - entry point aplikasi
-//
-// Cara compile:
-// g++ main.cpp fileOperations.cpp crudOperations.cpp menuInterface.cpp -o inventory.exe
-//
-// Cara run:
-// ./inventory.exe
-
 #include "inventory.h"
 #include <iostream>
-
 using namespace std;
 
 int main(){
